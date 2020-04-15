a85e65cd5d5cfbcea251896e54a6dd76dfbf0990
hildelbrandocorreasalcedo <63067085+hildelbrandocorreasalcedo@users.noreply.github.com>
2020-04-15 11:12:06 -0500

    empresaServiCreditos

---
+++ EmpresaServiCeditos/EmpresaServiCeditos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpresaServiCeditos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("creacion del repositorio,profe mirar las ramas");
+        }
+    }
+}
